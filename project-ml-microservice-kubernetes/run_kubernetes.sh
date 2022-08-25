994775adb4402cb8664fa4784d575c56f361aba5
Faith Odonghanro <toriboi.fo@gmail.com>
2022-08-25 01:05:45 +0000

    Implement task 5 to 7

--- project-ml-microservice-kubernetes/run_kubernetes.sh
+++ project-ml-microservice-kubernetes/run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath="toritseju/ml_predict"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+minikube kubectl -- create deployment k8s-ml-predict --image="${dockerpath}:latest" --port=80
 
 # Step 3:
 # List kubernetes pods
+minikube kubectl -- get pods
 
 # Step 4:
 # Forward the container port to a host
-
+minikube kubectl -- port-forward pods/k8s-ml-predict-7dc685d8cc-bldhs --address 0.0.0.0 8000:80
